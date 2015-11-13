a1863dc558eeecf9767933c8df973f2200505a16
gaoyb <gaoyb@meixing.com>
2015-11-13 10:35:52 +0800

    Modify error for

--- lib/china_sms/service/yunxin.rb
+++ lib/china_sms/service/yunxin.rb
@@ -1,44 +1,46 @@
-module ChinaSMS::Service::Yunxin
-  extend self
+module ChinaSMS::Service
+  module Yunxin
+    extend self
 
-  URL = 'http://h.1069106.com:1210/services/msgsend.asmx/SendMsg'
-  INTERVAL_TIME = 60  # 60 seconds for send sms interval time.
+    URL = 'http://h.1069106.com:1210/services/msgsend.asmx/SendMsg'
+    INTERVAL_TIME = 60  # 60 seconds for send sms interval time.
 
-  def to(phone, content, options)
-    send_with_block do
-      res = Net::HTTP.post_form(URI.parse(URL),
-                                userCode: options[:username],
-                                userPass: options[:password],
-                                DesNo: phone,
-                                Msg: content,
-                                Channel: 0)
-      result res.body
+    def to(phone, content, options)
+      send_with_block do
+        res = Net::HTTP.post_form(URI.parse(URL),
+                                  userCode: options[:username],
+                                  userPass: options[:password],
+                                  DesNo: phone,
+                                  Msg: content,
+                                  Channel: 0)
+        result res.body
+      end
     end
-  end
 
-  private
+    private
 
-  def send_with_block
-    @last_send_time ||= Time.at(0)
-    if (Time.now - @last_send_time) > INTERVAL_TIME
-      result = yield
-      @last_send_time = Time.now
-      result
-    else
+    def send_with_block
+      @last_send_time ||= Time.at(0)
+      if (Time.now - @last_send_time) > INTERVAL_TIME
+        result = yield
+        @last_send_time = Time.now
+        result
+      else
+        {
+            success: false,
+            code: "-999999"      #code -999999 indicates that sending failed is
+                                 # because interval time too short.
+        }
+      end
+
+    end
+
+    def result(body)
       {
-          success: false,
-          code: "-999999"      #code -999999 indicates that sending failed is
-                               # because interval time too short.
+        success: (body.to_i >= 0),
+        code: body
       }
     end
-
   end
-
-  def result(body)
-    {
-      success: (body.to_i >= 0),
-      code: body
-    }
-  end
-  
+    
 end
